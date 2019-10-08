23c74fbe1ecebcabbc70bda4d619af794e715cba
Uladzislau <48412863+kill1bit@users.noreply.github.com>
2019-10-08 16:09:54 +0300

    HomeWork1

---
+++ hm1/HomeWork1.java
@@ -0,0 +1,11 @@
+public class HomeWork1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println(3+22);
+        System.out.println("3"+22);
+        System.out.println("3"+2+2);
+        System.out.println("3"+(2+2));
+    }
+}
